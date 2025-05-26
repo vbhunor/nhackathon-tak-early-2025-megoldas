efef65e2de11c37d42208dba0a97a88f7c7a751d
Vigh-Bucz Hunor <73525702+vbhunor@users.noreply.github.com>
2025-05-26 18:45:34 +0200

    Update main.py

--- fibonacci/main.py
+++ fibonacci/main.py
@@ -1,4 +1,23 @@
 with open('./input.txt', 'r') as f:
-  input = f.read()
+    input = f.read().split()
+    lista = input
 
-print(input)
+for i in lista:
+    try:
+        n = int(i)
+        fi_old1 = 0
+        fi_old2 = 1
+        oszthato_3mal = []
+
+        for _ in range(n):
+            fi_new = fi_old1 + fi_old2
+            if fi_new % 3 == 0:
+                oszthato_3mal.append(str(fi_new))
+            fi_old1 = fi_old2
+            fi_old2 = fi_new
+
+        print(", ".join(oszthato_3mal) if oszthato_3mal else "N/A")
+    except ValueError:
+        print("N/A")
+
+print("Bemenet:", input)
